cf26bf3280f453318c9083297c1fe55d149dad60
Arthur Molnar <arthur.molnar@zenitech.co.uk>
2019-11-22 16:33:41 +0200

    implement feedback form

--- realestate/src/house/index.js
+++ realestate/src/house/index.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 import "./house.css";
-import PropTypes from 'prop-types';
+import emailIcon from './Email.png';
+import Inquiry from './Inquiry';
 
-class House extends Component {
-  state = { inquiryShown: false }
+class house extends Component {
+    state = { inquiryShown: false }
 
-  render() {
-    const house = this.props.house;
-    return (
-      <div>
-        <div className="row mt-2">
-          <h5 className="col-md-12">{house.country}</h5>
+    inquiryToggle = () => {
+        this.setState({ inquiryShown: !this.state.inquiryShown })
+      }
+
+    render() { 
+        const house = this.props.house;
+        const inquiry = this.state.inquiryShown ? 
+            <Inquiry house={house} /> : null;
+        return (
+        <div>
+            <div className="row mt-2">
+            <h5 className="col-md-12">{house.country}</h5>
+            </div>
+            <div className="row">
+            <h3 className="col-md-12">{house.address}</h3>
+            </div>
+            <div className="row">
+            <div className="col-md-7">
+                <img src={`https://images.pexels.com/photos/${house.photo}/pexels-photo-${house.photo}.jpeg?w=600&h=400&auto=compress&cs=tinysrgb`} alt="House" />
+            </div>
+            <div className="col-md-5">
+                <p className="price">${house.price}</p>
+                <p>{house.description}</p>
+                <img src={emailIcon} height="50" alt="inquiry" 
+                  onClick={this.inquiryToggle} />
+                {inquiry}
+            </div>
+            </div>      
         </div>
-        <div className="row">
-          <h3 className="col-md-12">{house.address}</h3>
-        </div>
-        <div className="row">
-          <div className="col-md-7">
-            <img src={`https://images.pexels.com/photos/${house.photo}/pexels-photo-${house.photo}.jpeg?w=600&h=400&auto=compress&cs=tinysrgb`} alt="House" />
-          </div>
-          <div className="col-md-5">
-            <p className="price">${house.price}</p>
-            <p>{house.description}</p>
-          </div>
-        </div>      
-      </div>
-    );
-  }
+        )
+    }
 }
-
-House.propTypes = {house: PropTypes.object.isRequired}
-export default House;
+ 
+export default house;
